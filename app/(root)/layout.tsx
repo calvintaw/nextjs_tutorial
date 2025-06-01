531a013d9eb541ec64b410939c72503af36868db
AmbitiousCalvin <190220508+AmbitiousCalvin@users.noreply.github.com>
2025-06-01 08:16:18 +0630

    Added local font and theme

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<main className="font-work-snas">
+		<main className="font-work-sans">
 			<Navbar></Navbar>
 			{children}
 		</main>
